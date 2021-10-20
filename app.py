4781d087d8d15c3e2abad0df82aed070fb67daa5
Jacob Coulter <jacob.coulter@gmail.com>
2021-10-19 20:59:07 -0400

    Adding temperature reading to flask app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from flask import Flask, render_template
 from flask import request
 
@@ -15,6 +15,24 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     # setupGPIO()
+    # TODO: make the devices properties
+    temp_store1 = open(
+        "/sys/bus/w1/devices/28-012063bee088/w1_slave")  # change this number to the Device ID of your sensor
+    temp_store1.close()
+    water_temperature = float(temp_store1.read().split("\n")[1].split(" ")[9][2:])
+    water_temperature = water_temperature / 1000
+    # Fahrenheit = (Celsius * 9 / 5) + 32
+    print("The water temperature is {}".format(water_temperature))
+
+    # TODO: make the devices properties
+    temp_store2 = open(
+        "/sys/bus/w1/devices/28-012063c43c9d/w1_slave")  # change this number to the Device ID of your sensor
+    temp_store2.close()
+    air_temperature = float(temp_store2.read().split("\n")[1].split(" ")[9][2:])
+    air_temperature = air_temperature / 1000
+    # Fahrenheit = (Celsius * 9 / 5) + 32
+    print("The air temperature is {}".format(air_temperature))
+
     if request.method == 'POST':
         print("I am posting!")
         # print initial pin status before evaluating and changing
